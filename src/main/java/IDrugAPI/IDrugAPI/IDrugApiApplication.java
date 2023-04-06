ecd218b8715e422bd442f14e2be6b420bf545c27
Sergey Shapovalov <Shapovalov.Sergey2016@yandex.ru>
2023-04-06 13:02:48 +0300

    Initial commit

---
+++ src/main/java/IDrugAPI/IDrugAPI/IDrugApiApplication.java
@@ -0,0 +1,15 @@
+package IDrugAPI.IDrugAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class IDrugApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IDrugApiApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/IDrugAPI/IDrugAPI/IDrugApiApplicationTests.java
@@ -0,0 +1,13 @@
+package IDrugAPI.IDrugAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IDrugApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
